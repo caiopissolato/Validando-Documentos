7b6039e648a828b7958d2e05d3be888af140109b
Caio Pissolato <caio_pissolato.05@hotmail.com>
2022-03-02 15:09:16 -0300

    Implementando o CNPJ

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from CPF import Cpf
+from cpf_cnpj import CpfCnpj
 
-cpf1 = Cpf("70095189807")
-print(cpf1)
+cpf1 = CpfCnpj("70095189807", "cpf")
+#print(cpf1)
+cnpj1 = "35379838000112"
+
+documento = CpfCnpj(cnpj1, "cnpj")
+print(documento,"\n",cpf1)
